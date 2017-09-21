856938566db910cef97a18422eaa1cb950e0574f
シモン <simon@azet.jp>
2017-09-21 12:34:14 +0900

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mouse_mover
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/mouse_mover/core_test.clj
@@ -0,0 +1,7 @@
+(ns mouse-mover.core-test
+  (:require [clojure.test :refer :all]
+            [mouse-mover.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
