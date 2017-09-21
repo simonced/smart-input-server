5932547e43b4f77f8034131aa8689892a2da0252
シモン <simon@azet.jp>
2017-09-21 13:07:08 +0900

    add first basic UDP server logic (infinit loop for now)

--- src/mouse_mover/core.clj
+++ src/mouse_mover/core.clj
@@ -1,6 +1,27 @@
 (ns mouse-mover.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; Ref: https://gist.github.com/stingh711/3760481
+;; ==============================================
+
+(import '(java.net DatagramSocket DatagramPacket))
+
+;; socket
+(def socket (DatagramSocket. 5200))
+
+(def running (atom true))
+(def buffer (make-array Byte/TYPE 1024))
+
+;; handling packet data
+(defn parse [packet]
+  (println (String. (.getData packet) 0 (.getLength packet))))
+;;; need to learn the meaning of that writing style...
+
+
+(defn start-server []
+  ;; Do something about how to stop that infinite loop!
+  (while (true? @running)
+    (let [packet (DatagramPacket. buffer 1024)]
+      (do
+        (println "running")
+        (.receive socket packet)
+        (future (parse packet))))))
